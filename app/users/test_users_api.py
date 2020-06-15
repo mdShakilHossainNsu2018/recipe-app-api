00958e86724b05a892f73f6437c47e09c2295ee1
mdShakilHossainNsu20!8 <shakilnsu2018@gmail.com>
2020-06-15 10:58:08 +0600

    add create token

--- app/users/test_users_api.py
+++ app/users/test_users_api.py
@@ -6,6 +6,7 @@ from rest_framework.test import APIClient
 from rest_framework import status
 
 CREATE_USER_URL = reverse('users:create')
+TOKEN_URL = reverse('users:token')
 
 
 def create_user(**kwargs):
@@ -39,7 +40,7 @@ class PublicUserApi(TestCase):
         create_user(**payload)
 
         res = self.client.post(CREATE_USER_URL, payload)
-        
+
         self.assertEquals(res.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_password_too_short(self):
@@ -62,3 +63,46 @@ class PublicUserApi(TestCase):
         user_exists = user.exists()
         self.assertFalse(user_exists)
 
+    def test_create_token_for_user(self):
+        payload = {
+            'email': 'test@gmail.com',
+            'password': 'testpass',
+        }
+        create_user(**payload)
+
+        res = self.client.post(TOKEN_URL, payload)
+        self.assertIn('token', res.data)
+        self.assertEquals(res.status_code, status.HTTP_200_OK)
+
+    def test_create_token_invalid_credentials(self):
+        create_user(email='test@gmail.com', password='testpass')
+        payload = {
+            'email': 'test@gmail.com',
+            'password': 'wrong',
+        }
+
+        res = self.client.post(TOKEN_URL, payload)
+        self.assertNotIn('token', res.data)
+        self.assertEquals(res.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_create_token_no_user(self):
+
+        payload = {
+            'email': 'test@gmail.com',
+            'password': 'testpass',
+        }
+
+        res = self.client.post(TOKEN_URL, payload)
+        self.assertNotIn('token', res.data)
+        self.assertEquals(res.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_create_token_missing_field(self):
+
+        payload = {
+            'email': 'test',
+            'password': '',
+        }
+
+        res = self.client.post(TOKEN_URL, payload)
+        self.assertNotIn('token', res.data)
+        self.assertEquals(res.status_code, status.HTTP_400_BAD_REQUEST)
--- app/users/views.py
+++ app/users/views.py
@@ -1,6 +1,14 @@
 from rest_framework.generics import CreateAPIView
-from .serializers import UserSerializer
+from .serializers import UserSerializer, AuthTokenSerializers
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
 
 
 class CreateUserView(CreateAPIView):
     serializer_class = UserSerializer
+
+
+class CreateTokenView(ObtainAuthToken):
+    serializer_class = AuthTokenSerializers
+
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
