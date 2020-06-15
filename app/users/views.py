f3fd5b801f92af05583b7716e684d8f127a77314
mdShakilHossainNsu20!8 <shakilnsu2018@gmail.com>
2020-06-15 12:12:13 +0600

    create user done

--- app/users/views.py
+++ app/users/views.py
@@ -1,7 +1,8 @@
-from rest_framework.generics import CreateAPIView
+from rest_framework.generics import CreateAPIView, RetrieveUpdateAPIView
 from .serializers import UserSerializer, AuthTokenSerializers
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.settings import api_settings
+from rest_framework import permissions, authentication
 
 
 class CreateUserView(CreateAPIView):
@@ -12,3 +13,12 @@ class CreateTokenView(ObtainAuthToken):
     serializer_class = AuthTokenSerializers
 
     renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+
+
+class ManageUserView(RetrieveUpdateAPIView):
+    serializer_class = UserSerializer
+    authentication_classes = (authentication.TokenAuthentication,)
+    permission_classes = (permissions.IsAuthenticated,)
+
+    def get_object(self):
+        return self.request.user
